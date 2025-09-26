9b2ea7f5f01c7df89521b2955cb6b5881c193014
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-09-26 09:19:01 +0100

    Initial Commit

---
+++ iosTakeHomeProject/iosTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTakeHomeProjectApp.swift
+//  iosTakeHomeProject
+//
+//  Created by Khawla Zarrami on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
