f4de1b65fe0a29c773061dec3d71bf08efa87450
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-11-19 13:19:05 +0100

    Clean code

---
+++ iosTakeHomeProject/Mocks/Create/NetworkingManagerCreateSuccessMock.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkingManagerCreateSuccessMock.swift
+//  iosTakeHomeProject
+//
+//  Created by Khawla Zarrami on 17/11/2025.
+//
+
+#if DEBUG
+import Foundation
+
+class NetworkingManagerCreateSuccessMock: NetworkingManagerImpl {
+    
+    func request<T>(session: URLSession, _ endPoint: iosTakeHomeProject.EndPoint, type: T.Type) async throws -> T where T : Decodable, T : Encodable {
+        return Data() as! T
+    }
+    
+    func request(session: URLSession, _ endpoint: iosTakeHomeProject.EndPoint) async throws {
+        
+    }
+    
+    
+}
+
+#endif
